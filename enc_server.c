dd97bc7b86db2aace3986fae173d6145836e3478
Marilyn Leary <marilyneleary@gmail.com>
2021-11-28 15:36:22 -0800

    Update enc_server.c

--- enc_server.c
+++ enc_server.c
@@ -8,8 +8,8 @@
 
 // Error function used for reporting issues
 void error(const char *msg) {
-  perror(msg);
-  exit(1);
+    perror(msg);
+    exit(1);
 } 
 
 
@@ -20,7 +20,7 @@ void error(const char *msg) {
  **/
 
 // Set up the address struct for the server socket
-void setupAddressStruct(struct sockaddr_in* address, 
+void setupAddressStructServer(struct sockaddr_in* address, 
                         int portNumber){
  
   // Clear out the address struct
@@ -42,6 +42,8 @@ int main(int argc, char *argv[]){
     int bufferLength = 0;
     struct sockaddr_in serverAddress, clientAddress;
     socklen_t sizeOfClientInfo = sizeof(clientAddress);
+    // for counting child processes; need to have up to 5
+    int counter = 0;
 
     // Check usage & args
     if (argc < 2) { 
@@ -56,7 +58,7 @@ int main(int argc, char *argv[]){
     }
 
     // Set up the address struct for the server socket
-    setupAddressStruct(&serverAddress, atoi(argv[1]));
+    setupAddressStructServer(&serverAddress, atoi(argv[1]));
 
     // Associate the socket to the port
     if (bind(listenSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
@@ -69,24 +71,169 @@ int main(int argc, char *argv[]){
     // Accept a connection, blocking if one is not available until one connects
     while(1) {
         // Accept the connection request which creates a connection socket
+        if (counter >=5) {
+            sleep(1000);
+            continue;
+        }
         connectionSocket = accept(listenSocket, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); 
         if (connectionSocket < 0){
             error("ERROR on accept");
         }
+        counter++;
 
-        //printf("SERVER: Connected to client running at host %d port %d\n", ntohs(clientAddress.sin_addr.s_addr), ntohs(clientAddress.sin_port));
+        int child;
+        pid_t spawnpid = fork();
+        if (spawnpid == -1) {                 // failed
+            fprintf(stderr, "Failed to fork.\n");
+            continue;
+        } else if (spawnpid > 0) {            // parent
+            continue;
+        } else if (spawnpid == 0) {           // child
+            // Get the message from the client and display it
+            memset(buffer, '\0', 256);
+            // Read the client's message from the socket
+            charsRead = recv(connectionSocket, buffer, 3, 0); 
+            if (charsRead < 0) {
+                counter--;
+                error("ERROR reading from socket");
+            }
+            if (strcmp(buffer, "enc") != 0) {
+                counter--;
+                fprintf(stderr, "Incorrect server connection");
+                exit(2);
+            }
+            printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+            
+            memset(buffer, '\0', 256);
+            // get the size of the mexsage
+            charsRead = recv(connectionSocket, buffer, 5, 0); 
+            if (charsRead < 0){
+                counter--;
+                error("ERROR writing to socket");
+            }
+            printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+            int sizeOfMsg = atoi(buffer);
 
-        // Get the message from the client and display it
-        memset(buffer, '\0', 256);
-        // Read the client's message from the socket
-        charsRead = recv(connectionSocket, buffer, 255, 0); 
-        if (charsRead < 0) {
-            error("ERROR reading from socket");
-        }
-        printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+            // get message
+            memset(buffer, '\0', 256);
+            // TODO allocate a buffer later
+            // TODO only read 256 bytes at a time
+            // TODO retry n number of times ???   
+            //charsRead = recv(connectionSocket, buffer, sizeOfMsg, 0);
+            char *msgBuffer = calloc(sizeOfMsg, sizeof(char));
+            int msgIndex = 0;
+            int len = sizeOfMsg;
+            while (len > 0) {
+                if (len < 256) {
+                    //sentChars = send(socketFD, &buffer[msgIndex], len, 0);
+                    charsRead = recv(connectionSocket, &msgBuffer[msgIndex], len, 0);
+                    if (charsRead != len) {
+                        error("Data is missing");
+                    }
+                    break;
+                } else {
+                    charsRead = recv(connectionSocket, &msgBuffer[msgIndex], 256, 0);
+                    msgIndex += 256;
+                    len = len - 256;
+                    if (charsRead != 256) {
+                        error("Data is missing");
+                    }
+                }
+            }
+
+            // redo for key
+
+            
+            
+            
+            if (charsRead < 0){
+                counter--;
+                error("ERROR writing to socket");
+            }
+            printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+            //char *msgBuffer = calloc(sizeOfMsg, sizeof(char));
+            // save message in buffer
+            //strcpy(msgBuffer, buffer);
+
+            // get key
+            memset(buffer, '\0', 256);
+            // get the size of the mexsage
+            charsRead = recv(connectionSocket, buffer, 5, 0); 
+            if (charsRead < 0){
+                counter--;
+                error("ERROR writing to socket");
+            }
+            printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+            int sizeOfKey = atoi(buffer);
+            
+            
+            //printf("server: rec'd buffer size is %d\n", sizeOfMsg);
+            // get message
+            memset(buffer, '\0', 256);
+            // TODO allocate a buffer later
+            // TODO only read 256 bytes at a time
+            // TODO retry n number of times ???
+            charsRead = recv(connectionSocket, buffer, sizeOfKey, 0);
+            if (charsRead < 0){
+                counter--;
+                error("ERROR writing to socket");
+            }
+            printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+            char *keyBuffer = calloc(sizeOfKey, sizeof(char));
+            // save message in buffer
+            strcpy(keyBuffer, buffer);
+
+
+            char *encBuffer = calloc(sizeOfMsg, sizeof(char));
+            int i = 0;
+            for (i = 0; i< sizeOfMsg; i++) {
+                char a, b;
+                if (msgBuffer[i] == ' ') {
+                    a = 26;
+                } else {
+                    a = msgBuffer[i] - 'A';        // convert to number, A = 0, B = 1, ... Z = 25
+                }
+
+                if (keyBuffer[i] == ' ') {
+                    b = 26;
+                } else {
+                    b = keyBuffer[i] - 'A';
+                }
+              
+                encBuffer[i] = (a + b) % 27;
+                if (encBuffer[i] == 26) {
+                    encBuffer[i] = ' ';
+                } else {
+                    encBuffer[i] = encBuffer[i] + 'A';
+                }
+            }
+
+            //printf("Encryption: %s\n", encBuffer);
+
+            // tell client how big the encrypted message will be
+            memset(buffer, '\0', sizeof(buffer));
+            sprintf(buffer, "%5d", sizeOfMsg);
+            int sentChars = send(connectionSocket, buffer, strlen(buffer), 0);
+            
+            // send message
+            memset(buffer, '\0', sizeof(buffer));
+            strcpy(buffer, encBuffer);
+            printf("SERVEr: sending \"%s\" \n", buffer);
+            sentChars = send(connectionSocket, buffer, strlen(buffer), 0);
+            
+/*
+            sentChars = send(connectionSocket, encBuffer, sizeof(encBuffer), 0);
+            if (keyRead < 0) {
+                error("ERROR writing key to socket");
+            }
+*/
+            counter--;
+            exit(0);
+        } 
 
 
         // get key
+        /*
         connectionSocket = accept(listenSocket, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); 
         if (connectionSocket < 0){
             error("ERROR on accept");
@@ -98,21 +245,12 @@ int main(int argc, char *argv[]){
             error("ERROR reading key from socket");
         }
         printf("SERVER: I received this from the client: \"%s\"\n", keyBuffer);
-        
-
-        // encrypt message
-        /*
-        int i = 0;
-        for (i = 0; i < sizeof(buffer); i++) {
-          encBuffer[i] = buffer[i] + keyBuffer[i];
-        }
         */
 
+        
+
         // Send a Success message back to the client
-        charsRead = send(connectionSocket, buffer, sizeof(buffer), 0); 
-        if (charsRead < 0){
-          error("ERROR writing to socket");
-        }
+        
 
         /*
         keyRead = send(connectionSocket, keyBuffer, sizeof(keyBuffer), 0);
